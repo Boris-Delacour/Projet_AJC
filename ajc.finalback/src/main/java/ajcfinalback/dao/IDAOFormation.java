eddf97406682af0264b5ddb30924ecc0cf72a151
LukasRiv <35290110+LukasRiv@users.noreply.github.com>
2024-07-10 16:54:02 +0200

    Gestionnaire Formation DAO Services TestU
    Gestionnaire Formation :
    
    Constructeurs
    IDAO
    Services
    Tests Unitaires

---
+++ ajc.finalback/src/main/java/ajcfinalback/dao/IDAOFormation.java
@@ -0,0 +1,9 @@
+package ajcfinalback.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ajcfinalback.model.Formation;
+
+public interface IDAOFormation extends JpaRepository<Formation,Integer>{
+
+}
---
+++ ajc.finalback/src/main/java/ajcfinalback/dao/IDAOGestionnaire.java
@@ -0,0 +1,9 @@
+package ajcfinalback.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ajcfinalback.model.Gestionnaire;
+
+public interface IDAOGestionnaire extends JpaRepository<Gestionnaire,Integer>{
+
+}
