8bf730e4d391055001a62b9df922ff9d6708743c
LukasRiv <35290110+LukasRiv@users.noreply.github.com>
2024-07-10 11:35:26 +0200

    Models Gestionnaire et Formation
    Ajout Models SANS annotations

---
+++ ajc.finalback/src/main/java/ajcfinalback/model/Gestionnaire.java
@@ -0,0 +1,79 @@
+package ajcfinalback.model;
+
+import java.util.List;
+
+public class Gestionnaire {
+
+	private int id;
+	private String lastName;
+	private String firstName;
+	private String email;
+	private String login;
+	private String password;
+	private List<Formation> formations;
+	
+	
+	public Gestionnaire() {
+		
+	}
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Formation> getFormations() {
+		return formations;
+	}
+
+	public void setFormations(List<Formation> formations) {
+		this.formations = formations;
+	}
+	
+	
+	
+}
