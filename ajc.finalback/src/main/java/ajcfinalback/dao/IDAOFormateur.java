6f6c1f03620f416e42adfd02e866d72291e73a83
Boris-Delacour <boris.delacour@outlook.fr>
2024-07-10 17:59:44 +0200

    Partie Formateur/Matiere
    Service, DAO et Test pour Formateur, Matiere et FormateurMatiere

---
+++ ajc.finalback/src/main/java/ajcfinalback/dao/IDAOFormateur.java
@@ -0,0 +1,9 @@
+package ajcfinalback.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ajcfinalback.model.Formateur;
+
+public interface IDAOFormateur extends JpaRepository<Formateur, Integer> {
+
+}
