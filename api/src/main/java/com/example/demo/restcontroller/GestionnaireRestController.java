672cf3e368ce8eaf20fccc8a3f2f3fa2c522f312
LukasRiv <35290110+LukasRiv@users.noreply.github.com>
2024-07-19 16:21:49 +0200

    GestionnaireRestController
    Gestionnaire :
    RestController
    Request
    Response

---
+++ api/src/main/java/com/example/demo/restcontroller/GestionnaireRestController.java
@@ -0,0 +1,80 @@
+package com.example.demo.restcontroller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.example.demo.dto.request.GestionnaireRequest;
+import com.example.demo.dto.response.GestionnaireResponse;
+import com.example.demo.model.Gestionnaire;
+import com.example.demo.service.GestionnaireService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/gestionnaire")
+// @SecurityRequirement(name="basicAuth")
+@CrossOrigin(origins = "*")
+public class GestionnaireRestController {
+
+	@Autowired
+    private GestionnaireService gSrv;
+
+
+    @GetMapping("/{id}")
+	// @JsonView(CustomJsonViews.StagaireWithFiliere.class)
+	public GestionnaireResponse getById(@PathVariable Integer id) {
+		return new GestionnaireResponse(gSrv.getById(id));
+	}
+
+    @GetMapping("")
+    // @JsonView(CustomJsonViews.Common.class)
+    public List<GestionnaireResponse> getAll() {
+        return gSrv.getAll().stream().map(gestionnaire -> new GestionnaireResponse(gestionnaire))
+                            .collect(Collectors.toList());
+    }
+
+    @PostMapping("")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    // @JsonView(CustomJsonViews.Common.class)
+    public GestionnaireResponse create(@Valid @RequestBody GestionnaireRequest gr, BindingResult br)  {
+        if (br.hasErrors()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        Gestionnaire gestionnaire = new Gestionnaire();
+        BeanUtils.copyProperties(gr, gestionnaire);
+        return new GestionnaireResponse(gSrv.insert(gestionnaire));
+    }
+
+    @PutMapping("/{id}")
+    // @JsonView(CustomJsonViews.Common.class)
+    public GestionnaireResponse update(@Valid @RequestBody GestionnaireRequest gr, BindingResult br, @PathVariable Integer id) {
+        if (br.hasErrors()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        Gestionnaire gestionnaire = gSrv.getById(id);
+        BeanUtils.copyProperties(gr, gestionnaire);
+        return new GestionnaireResponse(gSrv.update(gestionnaire));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable("id") Integer id){
+        gSrv.deleteById(id);
+    }
+}
