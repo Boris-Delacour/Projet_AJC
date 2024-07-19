44ee2fbb6706709400a29c53a345b1830c82c788
Safienz <tomblue764@gmail.com>
2024-07-19 12:53:49 +0200

    Ajout Technicien et matparformation RestController

---
+++ api/src/main/java/com/example/demo/dto/request/TechnicienRequest.java
@@ -0,0 +1,65 @@
+package com.example.demo.dto.request;
+
+public class TechnicienRequest {
+	
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String login;
+	private String password;
+
+	public TechnicienRequest() {
+
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+}
