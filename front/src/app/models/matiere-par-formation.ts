f70e689f144f2546dd60a3a6dbf3f5ebc409ae09
Alexandre  M <silfra6494@gmail.com>
2024-07-22 14:45:55 +0200

    model matiereParFormation

--- front/src/app/models/matiere-par-formation.ts
+++ front/src/app/models/matiere-par-formation.ts
@@ -1,2 +1,16 @@
+import { Formateur } from './formateur';
+import { Formation } from './formation';
+import { Matiere } from './matiere';
+import { Salle } from './salle';
+
 export class MatiereParFormation {
+  constructor(
+    public id?: number,
+    public start?: Date,
+    public end?: Date,
+    public matiere?: Matiere,
+    public formation?: Formation,
+    public formateur?: Formateur,
+    public salle?: Salle
+  ) {}
 }
