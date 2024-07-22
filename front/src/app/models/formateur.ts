f36273728823ca77b3eab141ffd9826993d575f9
Alexandre  M <silfra6494@gmail.com>
2024-07-22 12:35:27 +0200

    model formateur

---
+++ front/src/app/models/formateur.ts
@@ -0,0 +1,10 @@
+export class Formateur {
+  constructor(
+    public id?: number,
+    public lastname?: string,
+    public firstname?: string,
+    public email?: string,
+    public login?: string,
+    public password?: string
+  ) {}
+}
