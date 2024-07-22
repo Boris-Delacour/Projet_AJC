7be5b80ebc406724dc01f9d120706e9bf97cce76
Boris Delacour <boris.delacour@outlook.fr>
2024-07-22 14:41:26 +0200

    Update stagiaire.service.ts

--- front/src/app/services/stagiaire.service.ts
+++ front/src/app/services/stagiaire.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Stagiaire } from '../model/stagiaire';
+import { Stagiaire } from '../models/stagiaire';
 
 @Injectable({
   providedIn: 'root'
