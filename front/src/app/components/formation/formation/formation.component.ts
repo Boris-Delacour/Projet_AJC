d3774a34025d872f30955cb17cbc2731084d7c06
Aled <35290110+LukasRiv@users.noreply.github.com>
2024-07-22 18:00:52 +0200

    Formation Component Edit Service
    Formation :
    Component
    Edit
    Service
    Presque fini

---
+++ front/src/app/components/formation/formation/formation.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
+import { Formation } from '../../../models/formation';
+import { FormationService } from '../../../services/formation.service';
+
+@Component({
+  selector: 'app-formation',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './formation.component.html',
+  styleUrl: './formation.component.css'
+})
+export class FormationComponent  implements OnInit{
+  formations: Formation[] = [];
+
+  constructor(
+    public formationSrv: FormationService,
+    private activatedRoute: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+    this.initFormations();
+  }
+
+  initFormations() {
+    this.formationSrv.getAll().subscribe((formations) => {
+      this.formations = formations;
+    });
+  }
+
+  delete(id: number) {
+    this.formationSrv.deleteById(id).subscribe(() => {
+      this.initFormations();
+    });
+  }
+}
---
+++ front/src/app/services/formation.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Formation } from '../models/formation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormationService {
+  url = 'http://localhost:8080/ajcfinal/api/formation';
+  constructor(private httpClient: HttpClient) {}
+
+  private formationToFormationRequest(formation: Formation): any {
+    let obj = {
+      nom: formation.nom,
+      dateStart: formation.dateStart,
+      idGestionnaire: formation.gestionnaire?.id,
+      idFormateur: formation.formateur?.id,
+    };
+    return obj;
+  }
+
+  public getById(id: number): Observable<Formation> {
+    return this.httpClient.get<Formation>(`${this.url}/${id}`);
+  }
+
+  public getAll(): Observable<Formation[]> {
+    return this.httpClient.get<Formation[]>(this.url);
+  }
+
+  public create(formation: Formation): Observable<Formation> {
+    return this.httpClient.post<Formation>(
+      this.url,
+      this.formationToFormationRequest(formation)
+    );
+  }
+
+  public update(formation: Formation): Observable<Formation> {
+    return this.httpClient.put<Formation>(
+      `${this.url}/${formation.id}`,
+      this.formationToFormationRequest(formation)
+    );
+  }
+
+  public deleteById(id: number): Observable<void> {
+    return this.httpClient.delete<void>(`${this.url}/${id}`);
+  }
+
+  
+
+  
+}
