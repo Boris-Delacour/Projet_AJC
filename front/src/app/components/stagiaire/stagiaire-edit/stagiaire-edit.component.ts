9daa7bb1566521b52b014520932d437237ebdcda
CPacBellec <cbellec@normandiewebschool.fr>
2024-07-22 12:51:28 +0200

    start stagiaire model/comp/service

---
+++ front/src/app/components/stagiaire/stagiaire-edit/stagiaire-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stagiaire-edit',
+  templateUrl: './stagiaire-edit.component.html',
+  styleUrls: ['./stagiaire-edit.component.css']
+})
+export class StagiaireEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ front/src/app/services/stagiaire.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Stagiaire } from '../model/stagiaire';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StagiaireService {
+url = 'http://localhost:8080/' ;
+constructor(private httpClient: HttpClient) { }
+
+
+public getAll(): Observable<Stagiaire[]> {
+  return this.httpClient.get<Stagiaire[]>(this.url);
+}
+
+public create(stagiaire: Stagiaire): Observable<Stagiaire> {
+  return this.httpClient.post<Stagiaire>(
+    this.url,
+    this.stagiaireToStagiaireRequest(stagiaire)
+  );
+}
+
+private stagiaireToStagiaireRequest(stagiaire: Stagiaire): any {
+  let obj = {
+    firstName: stagiaire.firstName,
+    lastName: stagiaire.lastName,
+    email: stagiaire.email,
+    login: stagiaire.login,
+    password: stagiaire.password,
+    idFormation: stagiaire.formation?.id,
+    idOrdinateur: stagiaire.ordinateur?.id,
+  };
+  return obj;
+}
+
+public delete(id: number): Observable<void> {
+  return this.httpClient.delete<void>(`${this.url}/${id}`);
+}
+
+public getById(id: number): Observable<Stagiaire> {
+  return this.httpClient.get<Stagiaire>(`${this.url}/${id}`);
+}
+
+public update(stagiaire: Stagiaire): Observable<Stagiaire> {
+  return this.httpClient.put<Stagiaire>(
+    `${this.url}/${stagiaire.id}`,
+    this.stagiaireToStagiaireRequest(stagiaire)
+  );
+}
+
+}
