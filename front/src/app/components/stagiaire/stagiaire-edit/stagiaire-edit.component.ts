c17695646870d8d33f3743086d6ec4f10691dabe
CPacBellec <cbellec@normandiewebschool.fr>
2024-07-22 17:07:22 +0200

    html stagiaire + stagiaire-edit v0.1

--- front/src/app/components/stagiaire/stagiaire-edit/stagiaire-edit.component.ts
+++ front/src/app/components/stagiaire/stagiaire-edit/stagiaire-edit.component.ts
@@ -37,5 +37,48 @@ export class StagiaireEditComponent implements OnInit {
     public activatedroute: ActivatedRoute
   ) {}
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    // this.formationSrv.getAll().subscribe((formations) => {
+    //   this.formations = formations;
+    // });
+    this.formationsObservable = this.formationSrv.getAll();
+    this.ordinateursObservable = this.ordinateurSrv.getAll();
+    this.activatedroute.params.subscribe((params) => {
+      if (params['id']) {
+        this.stagiaireSrv.getById(params['id']).subscribe((stagiaire) => {
+          this.stagiaire = stagiaire;
+        });
+      }
+    });
+  }
+
+  save() {
+    if (this.stagiaire.id) {
+      this.stagiaireSrv.update(this.stagiaire).subscribe((stagiaire) => {
+        this.router.navigateByUrl('/stagiaire?q=update&id=' + stagiaire.id);
+      });
+    } else {
+      this.stagiaireSrv.create(this.stagiaire).subscribe((stagiaire) => {
+        this.router.navigateByUrl('/stagiaire?q=create&id=' + stagiaire.id);
+      });
+    }
+  }
+
+  compareFn(f1: Formation, f2: Formation): boolean {
+    //return f1 && f2 ? f1.id === f2.id : false;
+    if (f1 && f2) {
+      return f1.id === f2.id;
+    } else {
+      return false;
+    }
+  }
+
+  compareOr(o1: Ordinateur, o2: Ordinateur): boolean {
+    //return f1 && f2 ? f1.id === f2.id : false;
+    if (o1 && o2) {
+      return o1.id === o2.id;
+    } else {
+      return false;
+    }
+  }
 }
