84ae6d9d397846fddd77d490656edadc6e7fcab7
Safienz <tomblue764@gmail.com>
2024-07-22 14:04:56 +0200

    Model technicien + matiereparformation

---
+++ front/src/app/components/matiere-par-formation/matiere-par-formation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-matiere-par-formation',
+  standalone: true,
+  imports: [],
+  templateUrl: './matiere-par-formation.component.html',
+  styleUrl: './matiere-par-formation.component.css'
+})
+export class MatiereParFormationComponent {
+
+}
