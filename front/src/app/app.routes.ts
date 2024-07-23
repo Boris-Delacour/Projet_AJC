7fcd4bba0e3a460efbfd5bd512fc701edc00783c
Alexandre  M <silfra6494@gmail.com>
2024-07-23 14:24:32 +0200

    Maj route, ordinateur+video+salle

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -12,57 +12,57 @@ import { StagiaireComponent } from './components/stagiaire/stagiaire/stagiaire.c
 import { StagiaireEditComponent } from './components/stagiaire/stagiaire-edit/stagiaire-edit.component';
 import { TechnicienComponent } from './components/technicien/technicien.component';
 // import { TechnicienEditComponent } from './components/technicien/technicien-edit/technicien-edit.component';
-// import { OrdinateurComponent } from './components/ordinateur/ordinateur/ordinateur.component';
-// import { OrdinateurEditComponent } from './components/ordinateur/ordinateur-edit/ordinateur-edit.component';
-// import { SalleComponent } from './components/salle/salle/salle.component';
-// import { SalleEditComponent } from './components/salle/salle-edit/salle-edit.component';
-// import { SalleComponent } from './components/videoprojecteur/videoprojecteur/videoprojecteur.component';
-// import { SalleEditComponent } from './components/videoprojecteur/videoprojecteur-edit/videoprojecteur-edit.component';
 import { IndisponibiliteComponent } from './components/indisponibilite/indisponibilite/indisponibilite.component';
 import { IndisponibiliteEditComponent } from './components/indisponibilite/indisponibilite-edit/indisponibilite-edit.component';
 import { FormateurDetailsComponent } from './components/formateur/formateur-details/formateur-details.component';
+import { OrdinateurComponent } from './components/ordinateur/ordinateur/ordinateur.component';
+import { OrdinateurEditComponent } from './components/ordinateur/ordinateur-edit/ordinateur-edit.component';
+import { SalleComponent } from './components/salle/salle/salle.component';
+import { SalleEditComponent } from './components/salle/salle-edit/salle-edit.component';
+import { VideoprojecteurComponent } from './components/videoprojecteur/videoprojecteur/videoprojecteur.component';
+import { VideoprojecteurEditComponent } from './components/videoprojecteur/videoprojecteur-edit/videoprojecteur-edit.component';
 
 export const routes: Routes = [
-    {path: "formateur", component: FormateurComponent},
-    {path: "formateur/edit", component: FormateurEditComponent},
-    {path: "formateur/edit/:id", component: FormateurEditComponent},
-    {path: "formateur/details/:id", component: FormateurDetailsComponent},
+  { path: 'formateur', component: FormateurComponent },
+  { path: 'formateur/edit', component: FormateurEditComponent },
+  { path: 'formateur/edit/:id', component: FormateurEditComponent },
+  { path: 'formateur/details/:id', component: FormateurDetailsComponent },
 
-    {path: "matiere", component: MatiereComponent},
-    {path: "matiere/edit", component: MatiereEditComponent},
-    {path: "matiere/edit/:id", component: MatiereEditComponent},
+  { path: 'matiere', component: MatiereComponent },
+  { path: 'matiere/edit', component: MatiereEditComponent },
+  { path: 'matiere/edit/:id', component: MatiereEditComponent },
 
-    {path: "formation", component: FormationComponent},
-    {path: "formation/edit", component: FormationEditComponent},
-    {path: "formation/edit/:id", component: FormationEditComponent},
+  { path: 'formation', component: FormationComponent },
+  { path: 'formation/edit', component: FormationEditComponent },
+  { path: 'formation/edit/:id', component: FormationEditComponent },
 
-    {path: "matiereparformation", component: MatiereParFormationComponent},
+  { path: 'matiereparformation', component: MatiereParFormationComponent },
 
-    {path: "stagiaire", component: StagiaireComponent},
-    {path: "stagiaire/edit", component: StagiaireEditComponent},
-    {path: "stagiaire/edit/:id", component: StagiaireEditComponent},
+  { path: 'stagiaire', component: StagiaireComponent },
+  { path: 'stagiaire/edit', component: StagiaireEditComponent },
+  { path: 'stagiaire/edit/:id', component: StagiaireEditComponent },
 
-    {path: "technicien", component: TechnicienComponent},
-    // {path: "technicien/edit", component: TechnicienEditComponent},
-    // {path: "technicien/edit/:id", component: TechnicienEditComponent},
+  { path: 'technicien', component: TechnicienComponent },
+  // {path: "technicien/edit", component: TechnicienEditComponent},
+  // {path: "technicien/edit/:id", component: TechnicienEditComponent},
 
-    {path: "gestionnaire", component: GestionnaireComponent},
-    {path: "gestionnaire/edit", component: GestionnaireEditComponent},
-    {path: "gestionnaire/edit/:id", component: GestionnaireEditComponent},
+  { path: 'gestionnaire', component: GestionnaireComponent },
+  { path: 'gestionnaire/edit', component: GestionnaireEditComponent },
+  { path: 'gestionnaire/edit/:id', component: GestionnaireEditComponent },
 
-    // {path: "ordinateur", component: OrdinateurComponent},
-    // {path: "ordinateur/edit", component: OrdinateurEditComponent},
-    // {path: "ordinateur/edit/:id", component: OrdinateurEditComponent},
+  { path: 'ordinateur', component: OrdinateurComponent },
+  { path: 'ordinateur/edit', component: OrdinateurEditComponent },
+  { path: 'ordinateur/edit/:id', component: OrdinateurEditComponent },
 
-    // {path: "salle", component: SalleComponent},
-    // {path: "salle/edit", component: SalleEditComponent},
-    // {path: "salle/edit/:id", component: SalleEditComponent},
+  { path: 'salle', component: SalleComponent },
+  { path: 'salle/edit', component: SalleEditComponent },
+  { path: 'salle/edit/:id', component: SalleEditComponent },
 
-    // {path: "videoprojecteur", component: VideoprojecteurComponent},
-    // {path: "videoprojecteur/edit", component: VideoprojecteurEditComponent},
-    // {path: "videoprojecteur/edit/:id", component: VideoprojecteurEditComponent},
+  { path: 'videoprojecteur', component: VideoprojecteurComponent },
+  { path: 'videoprojecteur/edit', component: VideoprojecteurEditComponent },
+  { path: 'videoprojecteur/edit/:id', component: VideoprojecteurEditComponent },
 
-    {path: "indisponibilite", component: IndisponibiliteComponent},
-    {path: "indisponibilite/edit", component: IndisponibiliteEditComponent},
-    {path: "indisponibilite/edit/:id", component: IndisponibiliteEditComponent},
+  { path: 'indisponibilite', component: IndisponibiliteComponent },
+  { path: 'indisponibilite/edit', component: IndisponibiliteEditComponent },
+  { path: 'indisponibilite/edit/:id', component: IndisponibiliteEditComponent },
 ];
