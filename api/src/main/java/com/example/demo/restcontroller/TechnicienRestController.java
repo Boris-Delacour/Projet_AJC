3955456c8f9fd32f779aa8075799eea57d119214
Alexandre  M <silfra6494@gmail.com>
2024-07-25 15:56:35 +0200

    Technicien - ajout getById + correction put

--- api/src/main/java/com/example/demo/restcontroller/TechnicienRestController.java
+++ api/src/main/java/com/example/demo/restcontroller/TechnicienRestController.java
@@ -42,6 +42,12 @@ public class TechnicienRestController {
 		return tSrv.getAll().stream().map(t -> new TechnicienResponse(t)).collect(Collectors.toList());
 	}
 
+	@GetMapping("/{id}")
+	@JsonView(CustomJsonViews.Common.class)
+	public TechnicienResponse getById(@PathVariable("id") Integer id) {
+		return new TechnicienResponse(tSrv.getById(id));
+	}
+
 	@PostMapping("")
 	@JsonView(CustomJsonViews.Common.class)
 	@ResponseStatus(code = HttpStatus.CREATED)
@@ -61,6 +67,7 @@ public class TechnicienRestController {
 		}
 		Technicien technicien = tSrv.getById(id);
 		BeanUtils.copyProperties(tr, technicien);
+		technicien.setId(id);
 		return new TechnicienResponse(tSrv.update(technicien));
 	}
 
